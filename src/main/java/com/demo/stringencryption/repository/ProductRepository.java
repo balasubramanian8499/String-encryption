313b6ca6f8b8b1848aeaabea2f53e33c232573bb
Balasubramanian S <86341262+balasubramanian8499@users.noreply.github.com>
2024-10-02 15:42:35 +0530

    Adding src/main/java/com/demo/stringencryption/repository/ProductRepository.java

---
+++ src/main/java/com/demo/stringencryption/repository/ProductRepository.java
@@ -0,0 +1,10 @@
+package com.demo.stringencryption.repository;
+
+import com.demo.stringencryption.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product,Integer> {
+
+}
