c5e6d68d3b1659ffc3fd0e659db95eba35112e38
Balasubramanian S <86341262+balasubramanian8499@users.noreply.github.com>
2024-10-02 15:42:29 +0530

    Adding src/main/java/com/demo/stringencryption/controller/UserController.java

---
+++ src/main/java/com/demo/stringencryption/controller/UserController.java
@@ -0,0 +1,34 @@
+package com.demo.stringencryption.controller;
+
+import com.demo.stringencryption.response.UserDTO;
+import com.demo.stringencryption.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping
+    public ResponseEntity<?> saveUser(@RequestBody UserDTO userDto) {
+        return ResponseEntity.status(HttpStatus.OK).body(userService.saveUser(userDto));
+    }
+
+    @GetMapping
+    public ResponseEntity<?> getAllUsers() {
+        return ResponseEntity.ok(userService.getAllUsers());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getUserById(@PathVariable int id) {
+        return userService.getUserById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+}
