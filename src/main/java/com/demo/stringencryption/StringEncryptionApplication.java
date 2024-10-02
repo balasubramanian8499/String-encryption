ae23e3c34236d99e762048f370cf3153e2263b07
Balasubramanian S <86341262+balasubramanian8499@users.noreply.github.com>
2024-10-02 15:42:41 +0530

    Adding src/main/java/com/demo/stringencryption/StringEncryptionApplication.java

---
+++ src/main/java/com/demo/stringencryption/StringEncryptionApplication.java
@@ -0,0 +1,13 @@
+package com.demo.stringencryption;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StringEncryptionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StringEncryptionApplication.class, args);
+	}
+
+}
