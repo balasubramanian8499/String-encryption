a1e20d39ba93226c90db60f409d4ea0a0b72b85a
Balasubramanian S <86341262+balasubramanian8499@users.noreply.github.com>
2024-10-02 15:42:28 +0530

    Adding src/main/java/com/demo/stringencryption/controller/ProductController.java

---
+++ src/main/java/com/demo/stringencryption/controller/ProductController.java
@@ -0,0 +1,34 @@
+package com.demo.stringencryption.controller;
+
+import com.demo.stringencryption.model.Product;
+import com.demo.stringencryption.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/product")
+public class ProductController {
+
+    @Autowired
+    private ProductService productService;
+
+    @PostMapping
+    public ResponseEntity<?> saveProduct(@RequestBody Product product) {
+        return ResponseEntity.status(HttpStatus.OK).body(productService.saveProduct(product));
+    }
+
+    @GetMapping
+    public ResponseEntity<?> getAllProducts() {
+        return ResponseEntity.ok(productService.getAllProducts());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getProductById(@PathVariable int id) {
+        return productService.getProductById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+}
